8cd3cfbe853f2944a8daa92eae16c2e802b61b73
rchozas <rchglez@gmail.com>
2024-04-28 12:47:52 +0200

    cambios de L13 a P5

--- P5/red.js
+++ P5/red.js
@@ -32,6 +32,78 @@ class Nodo {
   conectar(nodo, peso) {
     this.conexiones.push({ nodo, peso });
   }
+  // Método para saber si un nodo está en la lista de conexiones de otro
+  isconnected(idn) {
+
+    let isconnected = false;
+
+    this.conexiones.forEach(({ nodo: conexion, peso }) => {      
+      if (idn == conexion.id) {
+        //console.log("id nodo conectado:" + conexion.id);
+        isconnected = true;
+      }      
+    });
+    
+    return isconnected;
+  }
+  // Método para saber la distancia entre dos nodos
+  node_distance(nx, ny) {
+
+    var a = nx - this.x;
+    var b = ny - this.y;
+        
+    return Math.floor(Math.sqrt( a*a + b*b ));
+
+  }
+  // Método para encontrar el nodo más alejado
+  far_node( nodos ) {
+
+    let distn = 0;
+    let cnode = this.id;
+    let distaux = 0;
+    let pos = 0;
+    let npos = 0;
+
+    for (let nodo of nodos) {
+      distaux = this.node_distance(nodo.x, nodo.y);
+  
+      if (distaux != 0 && distaux > distn) {
+        distn = distaux;
+        cnode = nodo.id;
+        npos = pos;
+      }
+
+      pos += 1;
+    }
+  
+    return {pos: npos, id: cnode, distance: distn,};
+
+  }
+  // Método para encontrar el nodo más cercano
+  close_node( nodos ) {
+
+    let far_node = this.far_node( nodos );
+    let cnode = far_node.id;
+    let distn = far_node.distance;
+    let distaux = 0;
+    let pos = 0;
+    let npos = 0;    
+  
+    for (let nodo of nodos) {
+      distaux = this.node_distance(nodo.x, nodo.y);
+  
+      if (distaux != 0 && distaux <= distn) {
+        distn = distaux;
+        cnode = nodo.id;
+        npos = pos;
+      }
+
+      pos += 1;
+    }
+  
+    return {pos:npos, id: cnode, distance: distn,}
+  
+  }
 
 }
   
@@ -41,26 +113,75 @@ function crearRedAleatoriaConCongestion(numNodos, numConexiones) {
   const nodos = [];
   let x = 0, y = 0, delay = 0;
   let nodoActual = 0, nodoAleatorio = 0, pickNode = 0, peso = 0;
+  let bSpace = false;
+
+  const xs = Math.floor(canvas.width / numNodos);
+  const ys = Math.floor(canvas.height / 2 );
+  const xr = canvas.width - nodeRadius;
+  const yr = canvas.height - nodeRadius;
+  let xp = nodeRadius;
+  let yp = nodeRadius;
+  let xsa = xs;
+  let ysa = ys;
 
   // Generamos los nodos
   for (let i = 0; i < numNodos; i++) {
-    x = randomNumber(nodeRadius, (canvas.width - nodeRadius)); // Generar coordenada x aleatoria
-    y = randomNumber(nodeRadius, (canvas.height - nodeRadius)); // Generar coordenada y aleatoria
+
+    //var random_boolean = Math.random() < 0.5;
+    if (Math.random() < 0.5) {
+      yp = nodeRadius;
+      ysa = ys;
+    } 
+    else {
+      yp = ys;
+      ysa = yr;
+    }
+
+    x = randomNumber(xp, xsa); // Generar coordenada x aleatoria
+    y = randomNumber(yp, ysa); // Generar coordenada y aleatoria
+
+    xp = xsa;
+    xsa = xsa + xs;
+
+    if ( xsa > xr && xsa <= canvas.width ) {
+      xsa = xr;
+    }
+
+    if ( xsa > xr && xsa < canvas.width ) {
+      xp = nodeRadius;
+      xsa = xs;
+    }    
+
     delay = generarRetardo(); // Retardo aleatorio para simular congestión
     nodos.push(new Nodo(i, x, y, delay)); // Generar un nuevo nodo y añadirlo a la lista de nodos de la red
   }
-
   // Conectamos los nodos
-  for (let i = 0; i < numNodos; i++) {
-    nodoActual = nodos[i];
-    for (let j = 0; j < numConexiones; j++) {
-      pickNode = Math.floor(Math.random() * numNodos);
-      nodoAleatorio = nodos[pickNode];
-      //peso = Math.random() * pipeRandomWeight; // Peso aleatorio para simular la distancia entre nodos
-      peso = pipeRandomWeight; // El mismo peso para todas las conexiones
-      nodoActual.conectar(nodoAleatorio, peso);
-    }
-  }
+  // Seleccionamos los nodos más cercanos teniendo en cuenta la distancia
+  // Seleccionamos tantos nodos como indica la variable numConexiones
+  // El nodo será candidato siempre que no estén ya conectados
+  for (let nodo of nodos) {
+    //console.log("id: " + nodo.id + " distancia al nodo: " + nodo.node_distance(nodos[0].x, nodos[0].y));
+ 
+     const clonedArray = [...nodos];
+ 
+     for (let j = 0; j < numConexiones; j++) {
+       let close_node = nodo.close_node(clonedArray);
+       //console.log(close_node);
+ 
+       if (!nodo.isconnected(close_node.id) && !clonedArray[close_node.pos].isconnected(nodo.id)) {
+         // Añadimos una nueva conexión
+         // Con el nodo más cercano y la distancia a ese nodo como el peso de la conexión
+         nodo.conectar(clonedArray[close_node.pos], close_node.distance);
+       }
+ 
+       // Eliminamos el nodo seleccionado del array clonado para evitar que 
+       // vuelva a salir elegido con splice.
+       // 0 - Inserta en la posición que le indicamos.
+       // 1 - Remplaza el elemento, y como no le damos un nuevo elemento se queda vacío.      
+       clonedArray.splice(close_node.pos, 1);
+     }
+ 
+   }
 
   return nodos;
 }
@@ -75,6 +196,7 @@ function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
+
 // Dibujar la red en el canvas
 function drawNet(nnodes) {
   // Dibujamos las conexiones entre nodos
@@ -84,6 +206,15 @@ function drawNet(nnodes) {
       ctx.moveTo(nodo.x, nodo.y);
       ctx.lineTo(conexion.x, conexion.y);
       ctx.stroke();
+
+      ctx.font = '12px Arial';
+      ctx.fillStyle = 'black';
+      ctx.textAlign = 'center';
+      pw = "N" + nodo.id + " pw " + peso;
+      const midX = Math.floor((nodo.x + conexion.x)/2);
+      const midY = Math.floor((nodo.y + conexion.y)/2);
+      ctx.fillText(pw, midX, midY);  
+
     });
   });
 
@@ -103,7 +234,6 @@ function drawNet(nnodes) {
     ctx.fillText(nodoDesc, nodo.x, nodo.y + 5);
   });
 }
-
 // Función de calback para generar la red de manera aleatoria
 btnCNet.onclick = () => {
 
